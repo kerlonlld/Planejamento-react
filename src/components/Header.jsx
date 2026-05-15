873ecffca389c40b503304032b95da614ebd1d4d
kerlonlld <kerlonluan40@gmail.com>
2026-05-15 14:21:10 -0300

    Adiciona projeto React completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,35 +16,37 @@ function Header({ usuario, login, logout, setSecao, toggleCalc }) {
         <span className="subtitle">TECHNOLOGY</span>
       </div>
 
-      <button className="menu-toggle" onClick={() => setMenuAberto(!menuAberto)}>
-        {menuAberto? <FaTimes /> : <FaBars />}
-      </button>
+      <div className="header-direita">
+        <button className="menu-toggle" onClick={() => setMenuAberto(!menuAberto)}>
+          {menuAberto? <FaTimes /> : <FaBars />}
+        </button>
 
-      <nav className={`navegacao ${menuAberto? 'ativo' : ''}`}>
-        <ul className="menu">
-          <li><a className="link_menu" onClick={() => trocaSecao('inicio')}>início</a></li>
-          <li><a className="link_menu" onClick={() => trocaSecao('financas')}>finanças</a></li>
-          <li><a className="link_menu" onClick={() => trocaSecao('investimentos')}>investimentos</a></li>
-          <li><a className="link_menu" onClick={() => trocaSecao('lazer')}>lazer</a></li>
-          <li><a className="link_menu" onClick={() => trocaSecao('imoveis')}>imóveis</a></li>
-          <li><a className="link_menu" onClick={() => trocaSecao('automoveis')}>automóveis</a></li>
-          <li>
-            <a className="link_menu" onClick={() => {toggleCalc(); setMenuAberto(false)}}>
-              <FaCalculator /> calculadora
-            </a>
-          </li>
-        </ul>
-      </nav>
+        <nav className={`navegacao ${menuAberto? 'ativo' : ''}`}>
+          <ul className="menu">
+            <li><a className="link_menu" onClick={() => trocaSecao('inicio')}>início</a></li>
+            <li><a className="link_menu" onClick={() => trocaSecao('financas')}>finanças</a></li>
+            <li><a className="link_menu" onClick={() => trocaSecao('investimentos')}>investimentos</a></li>
+            <li><a className="link_menu" onClick={() => trocaSecao('lazer')}>lazer</a></li>
+            <li><a className="link_menu" onClick={() => trocaSecao('imoveis')}>imóveis</a></li>
+            <li><a className="link_menu" onClick={() => trocaSecao('automoveis')}>automóveis</a></li>
+            <li>
+              <a className="link_menu" onClick={() => {toggleCalc(); setMenuAberto(false)}}>
+                <FaCalculator /> calculadora
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="header2">
-        {usuario? (
-          <>
-            <span className="user-name">Olá, {usuario.displayName?.split(' ')[0]}</span>
-            <button className="button321" onClick={logout}>Sair</button>
-          </>
-        ) : (
-          <button className="button321" onClick={login}>Login</button>
-        )}
+        <div className="user-info">
+          {usuario? (
+            <>
+              <span className="user-name">Olá, {usuario.displayName?.split(' ')[0]}</span>
+              <button className="button321" onClick={logout}>Sair</button>
+            </>
+          ) : (
+            <button className="button321" onClick={login}>Login</button>
+          )}
+        </div>
       </div>
     </header>
   )
